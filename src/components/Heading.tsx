d2c4a6b5487ee1d2332b75681a5d2df44bb448c6
123Somrat <jafaruddin875@gmail.com>
2024-06-25 13:53:08 +0200

    Consume state value useing useSelector

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,8 +1,12 @@
+import { RootState } from "../redux/store/store";
+import { useSelector } from "react-redux";
+
 export default function Heading({ title }: { title: string }) {
+   const count = useSelector((state:RootState)=>state.counter.value)
   return (
     <>
       <h1>{title}</h1>
-      <h1>sam</h1>
+      <h1>{count}</h1>
     </>
   );
 }
