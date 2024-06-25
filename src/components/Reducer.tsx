4382ea23e338ddbb9d20978cd3e8492e01f680d0
123Somrat <jafaruddin875@gmail.com>
2024-06-25 13:48:59 +0200

    Provide the store in whole app and dispath increment

--- src/components/Reducer.tsx
+++ src/components/Reducer.tsx
@@ -1,16 +1,17 @@
 
+import { useDispatch } from 'react-redux';
 import { increement } from '../redux/slice/counterSlice';
 
 export default function Reducer() {
   //const [state,dispatch] = useReducer(reducer,initialState)
-   
+   const dispatch = useDispatch()
 
 
 
   return (
     <>
       <h1>Cart</h1>
-      <button  onClick={}>Add Item</button>
+      <button  onClick={()=>dispatch(increement())}>Add Item</button>
       <button >Remove Item</button>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./components/Router.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
